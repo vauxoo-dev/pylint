6a94e3cfdb79231de91b15bd65d45ec4edf656fe
Moisés López <moylop260@vauxoo.com>
2016-05-17 23:47:47 +0000

    [FIX]git statusgit status continue

--- pylint/test/functional/wrong_import_position2.py
+++ pylint/test/functional/wrong_import_position2.py
@@ -1,10 +1,8 @@
 """Checks import order rule with nested non_import sentence"""
 # pylint: disable=unused-import,relative-import,ungrouped-imports,import-error,no-name-in-module,relative-beyond-top-level
 try:
-    # imports nested is a non_import sentence
     from sys import argv
 except ImportError:
     pass
 
-# TODO: Flake8 don't show error in this case
 import os
