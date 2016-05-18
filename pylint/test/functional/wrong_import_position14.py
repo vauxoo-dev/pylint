aaed018348d08a2a622af913af47df649399cde6
Moisés López <moylop260@vauxoo.com>
2016-05-18 01:09:16 +0000

    add bad cases

--- pylint/test/functional/wrong_import_position14.py
+++ pylint/test/functional/wrong_import_position14.py
@@ -1,4 +1,5 @@
 """Checks import position rule"""
 # pylint: disable=unused-import,relative-import,ungrouped-imports,import-error,no-name-in-module,relative-beyond-top-level
-a = 1
-import os
+if x:
+    import os
+import y
