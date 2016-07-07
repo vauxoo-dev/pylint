e97c56ce158d55ea70f2821e5afadeec23cc9604
Moisés López <moylop260@vauxoo.com>
2016-07-07 19:05:06 +0000

    [REF] test consider-merging-isinstance: Add more hard cases

--- pylint/test/functional/consider_merging_isinstance.py
+++ pylint/test/functional/consider_merging_isinstance.py
@@ -15,14 +15,16 @@ def isinstances():
     if isinstance(var[3], int) or isinstance(var[3], float) or isinstance(var[3], list) and True:  # [consider-merging-isinstance]
         pass
     result = isinstance(var[4], int) or isinstance(var[4], float) or isinstance(var[5], list) and False  # [consider-merging-isinstance]
-    result = isinstance(var[7], int) or not isinstance(var[7], float)
+
     result = isinstance(var[5], int) or True or isinstance(var[5], float)  # [consider-merging-isinstance]
 
     infered_isinstance = isinstance
     result = infered_isinstance(var[6], int) or infered_isinstance(var[6], float) or infered_isinstance(var[6], list) and False   # [consider-merging-isinstance]
+    result = isinstance(var[10]) or isinstance(var[10], int) and var[8] * 14 or isinstance(var[10], float) and var[5] * 14.4 or isinstance(var[10], list)   # [consider-merging-isinstance]
+
+    # not merged but valid
+    result = isinstance(var[5], int) and var[5] * 14 or isinstance(var[5], float) and var[5] * 14.4
+    result = isinstance(var[7], int) or not isinstance(var[7], float)
     result = isinstance(var[6], int) or isinstance(var[7], float)
     result = isinstance(var[6], int) or isinstance(var[7], int)
-
-    # not merged but valid because have a logic that depends of the result
-    result = isinstance(var[5], int) and var[5] * 14 or isinstance(var[5], float) and var[5] * 14.4
     return result
