fe3b4fa70bdf1c01409bf42cd26ce2a26ac98c06
Moisés López <moylop260@vauxoo.com>
2016-07-07 17:04:09 +0000

    [REF] test consider-merging-isinstance: Fix undefined-variable

--- pylint/test/functional/consider_merging_isinstance.py
+++ pylint/test/functional/consider_merging_isinstance.py
@@ -1,25 +1,25 @@
 """Checks use of consider-merging-isinstance"""
-# pylint:disable=line-too-long,bad-option-value,unpacking-non-sequence
+# pylint:disable=line-too-long
 
 
 def isinstances():
     "Examples of isinstances"
-    var1, var2, var3, var4, var5, var6, var7 = 85
+    var = range(10)
 
     # merged
-    if isinstance(var1, (int, long)):
+    if isinstance(var[1], (int, float)):
         pass
-    result = isinstance(var2, (int, long))
+    result = isinstance(var[2], (int, float))
 
     # not merged
-    if isinstance(var3, int) or isinstance(var3, long) or isinstance(var3, list) and True:  # [consider-merging-isinstance]
+    if isinstance(var[3], int) or isinstance(var[3], float) or isinstance(var[3], list) and True:  # [consider-merging-isinstance]
         pass
-    result = isinstance(var4, int) or isinstance(var4, long) or isinstance(var5, list) and False  # [consider-merging-isinstance]
-    result = isinstance(var7, int) or not isinstance(var7, long)
-    result = isinstance(var5, int) or True or isinstance(var5, long)  # [consider-merging-isinstance]
+    result = isinstance(var[4], int) or isinstance(var[4], float) or isinstance(var[5], list) and False  # [consider-merging-isinstance]
+    result = isinstance(var[7], int) or not isinstance(var[7], float)
+    result = isinstance(var[5], int) or True or isinstance(var[5], float)  # [consider-merging-isinstance]
 
     infered_isinstance = isinstance
-    result = infered_isinstance(var6, int) or infered_isinstance(var6, long) or infered_isinstance(var6, list) and False   # [consider-merging-isinstance]
-    result = isinstance(var6, int) or isinstance(var7, long)
-    result = isinstance(var6, int) or isinstance(var7, int)
+    result = infered_isinstance(var[6], int) or infered_isinstance(var[6], float) or infered_isinstance(var[6], list) and False   # [consider-merging-isinstance]
+    result = isinstance(var[6], int) or isinstance(var[7], float)
+    result = isinstance(var[6], int) or isinstance(var[7], int)
     return result
